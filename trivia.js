0fd23a98bc93045b7044d1c080d7abd7fab7aead
AngelhtProgrammer <angelht1311@gmail.com>
2025-06-17 00:09:55 -0400

    add different score

--- trivia.js
+++ trivia.js
@@ -1,5 +1,6 @@
 const body = document.querySelector('body')
 const todo = document.getElementById('centro');
+const flecha = document.getElementById('flecha');
 const resultados = document.createElement('div');
 resultados.setAttribute('id','resultadosT');
 const result = () =>
@@ -20,11 +21,39 @@ const result = () =>
 	if(document.getElementById('correcta5').checked)
 		{	score++;
 		}
+	if(document.getElementById('correcta6').checked)
+		{	score++;
+		}
         todo.remove();
-        resultados.innerText = `Tu puntaje es de: ${+score}`;
+       if ( +score == 0) {
+		resultados.innerText = `${+score}/6 Mediocre 💀`;
         body.appendChild(resultados);
+	   }
+       if ( +score == 1) {
+		resultados.innerText = `${+score}/6 decepcionante 💩`;
+        body.appendChild(resultados);
+	   }
+       if ( +score == 2) {
+		resultados.innerText = `${+score}/6 Qué malo 🤷‍♂️`;
+        body.appendChild(resultados);
+	   }
+       if ( +score == 3) {
+		resultados.innerText = `${+score}/6 Meh 👍`;
+        body.appendChild(resultados);
+	   }
+       if ( +score == 4) {
+ 		resultados.innerText = `${+score}/6 Bueno 😁`;
+        body.appendChild(resultados);
+	   }
+       if ( +score == 5) {
+ 		resultados.innerText = `${+score}/6 La rompiste 🤯`;
+        body.appendChild(resultados);
+	   }
+       if ( +score == 6) {
+ 		resultados.innerText = `${+score}/6 Perfecto 🎩`;
+        body.appendChild(resultados);
+	   }
 
-    
 
 };
 
